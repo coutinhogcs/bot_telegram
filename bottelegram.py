cdbf07a5a74d84fc40ca2e42072b4c5bf63be10e
Guilherme Coutinho <coutinhocguilherme@gmail.com>
2025-10-24 11:51:38 -0300

    Bot de emprego

---
+++ bottelegram.py
@@ -0,0 +1,95 @@
+import telebot 
+
+API_TOKEN = '8411488510:AAEOBznnlPVW1MfqXu4VP5sgk-cNYNP6i9g'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def opcao1(mensagem):
+   texto = f"""Olá {mensagem.chat.first_name} seja bem-vindo ao Bot de Vagas de Emprego em Tecnologia!
+Aqui você encontrará as melhores oportunidades de trabalho na área de tecnologia.
+Digite qualquer mensagem para ver as opções disponíveis.
+   """
+   bot.reply_to(mensagem, texto)
+
+
+
+@bot.message_handler(commands=['opcao1'])
+def opcao1(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de FrontEnd:
+https://portal.gupy.io/job-search/term=Frontend
+   """
+   bot.reply_to(mensagem, texto)
+
+
+@bot.message_handler(commands=['opcao2'])
+def opcao2(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de BackEnd:
+https://portal.gupy.io/job-search/term=Backend&workplaceTypes[]=remote
+   """
+   bot.reply_to(mensagem, texto)
+
+
+@bot.message_handler(commands=['opcao3'])
+def opcao3(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de FullStack:
+https://portal.gupy.io/job-search/term=Fullstack&workplaceTypes[]=remote
+   """
+   bot.reply_to(mensagem, texto)
+
+
+@bot.message_handler(commands=['opcao4'])
+def opcao4(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de Mobile:
+https://portal.gupy.io/job-search/term=Mobile&workplaceTypes[]=remote
+   """
+   bot.reply_to(mensagem, texto)
+
+@bot.message_handler(commands=['opcao5'])
+def opcao5(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de Data Science:
+https://portal.gupy.io/job-search/term=Data%20Science&workplaceTypes[]=remote
+   """ 
+   bot.reply_to(mensagem, texto)
+
+@bot.message_handler(commands=['opcao6'])
+def opcao6(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de DevOps:
+https://portal.gupy.io/job-search/term=DevOps&workplaceTypes[]=remote
+   """ 
+   bot.reply_to(mensagem, texto) 
+
+@bot.message_handler(commands=['opcao7'])
+def opcao7(mensagem):
+   texto = """Aqui estão os links com as melhores vagas de Analista de Banco de Dados:
+https://portal.gupy.io/job-search/term=Analista%20de%20Banco%20de%20Dados&workplaceTypes[]=remote,on-site,hybrid
+   """   
+   bot.reply_to(mensagem, texto)
+
+def verificar(mensagem):
+   return True
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    texto = """
+    Escolha uma opção para continuar:
+    
+/opcao1 Vagas de FrontEnd
+
+/opcao2 Vagas de BackEnd
+
+/opcao3 Vagas de FullStack
+
+/opcao4 Vagas de Mobile
+
+/opcao5 Vagas de Data Science
+
+/opcao6 Vagas de DevOps
+
+/opcao7 Vagas de Analista de Banco de Dados
+
+clique para selecionar uma opção.
+    """
+    bot.reply_to(mensagem, texto)
+
+bot.polling()
