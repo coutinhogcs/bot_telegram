4a754d782cdba37c43687bca9346026217020f60
Guilherme Coutinho <coutinhocguilherme@gmail.com>
2025-10-24 15:57:05 -0300

    manutenção

--- bottelegram.py
+++ bottelegram.py
@@ -2,6 +2,7 @@ from flask import Flask
 from threading import Thread
 from bot_instance import bot
 import os
+import time
 import handlers_begin
 import handlers_frontend
 import handlers_backend
@@ -23,10 +24,20 @@ def run_flask():
     app.run(host='0.0.0.0', port=port)
 
 
-
+# --- AQUI ESTÁ A MUDANÇA ---
 def seu_bot_principal():
     print("Iniciando o bot do Telegram...")
-    bot.polling()
+    
+    while True: # <-- 2. Adiciona um loop infinito
+        try:
+            # 3. Tenta rodar o polling (non_stop=True é uma boa prática)
+            bot.polling(non_stop=True, interval=0) 
+        
+        except Exception as e:
+            # 4. Se der QUALQUER erro (ConnectionError, etc.)
+            print(f"Erro no polling: {e}")
+            print("Reiniciando o bot em 5 segundos...")
+            time.sleep(5) # 5. Espera 5 segundos antes de tentar de novo
 
 
 
--- handlers_begin.py
+++ handlers_begin.py
@@ -2,7 +2,7 @@ from bot_instance import bot
 
 @bot.message_handler(commands=['start'])
 def opcao1(mensagem):
-   texto = f"""Olá {mensagem.chat.first_name} seja bem-vindo ao Bot de Vagas de Emprego em Tecnologia!
+   texto = f"""Olá {mensagem.chat.first_name}, seja bem-vindo ao Bot de Vagas de Emprego em Tecnologia!
 
 Aqui você encontrará as melhores oportunidades de trabalho na área de tecnologia.
 
